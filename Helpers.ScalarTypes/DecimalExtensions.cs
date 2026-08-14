705ec6f315179c5aefddf93850c0b482c6a4d119
Oleg Zhuravskiy <zhyravskiy.o@gmail.com>
2026-08-14 23:16:26 +0300

    [feat] [Helpers.ScalarTypes] added readme.md and fix documentation.

--- Helpers.ScalarTypes/DecimalExtensions.cs
+++ Helpers.ScalarTypes/DecimalExtensions.cs
@@ -4,8 +4,8 @@ public static class DecimalExtensions
 {
     extension(decimal number)
     {
-        /// <summary>Calculates the number of digits to the left of the decimal separator.</summary>
-        /// <returns>The count of integer digits. Returns 1 for values between -1 and 1.</returns>
+        /// <summary>Gets the number of digits in the integer part of the decimal value.</summary>
+        /// <returns>The number of digits before the decimal separator. Returns 1 for values between -1 and 1.</returns>
         public int GetDigitsBeforeSeparator()
         {
             decimal absoluteValue = decimal.Abs(number);
@@ -18,16 +18,16 @@ public static class DecimalExtensions
             return (int)Math.Floor(Math.Log10(Convert.ToDouble(absoluteValue))) + 1;
         }
 
-        /// <summary>Calculates the total number of digits (integer part + fractional part).</summary>
-        /// <returns>The total count of significant digits.</returns>
+        /// <summary>Gets the total number of digits before and after the decimal separator.</summary>
+        /// <returns>The total number of digits represented by the decimal value.</returns>
         public int GetDigits()
         {
             return number.GetDigitsBeforeSeparator() + number.GetDecimal();
         }
 
-        /// <summary>Extracts the number of decimal places from the numeric value.</summary>
-        /// <remarks>Converts the value to a decimal and inspects its internal scaling factor.</remarks>
-        /// <returns>The number of digits after the decimal point.</returns>
+        /// <summary>Gets the number of digits after the decimal separator.</summary>
+        /// <remarks>The number of decimal places is determined from the internal scale stored in the <see cref="decimal"/> value.</remarks>
+        /// <returns>The number of digits after the decimal separator.</returns>
         public int GetDecimal()
         {
             int[] bits = decimal.GetBits(number);
