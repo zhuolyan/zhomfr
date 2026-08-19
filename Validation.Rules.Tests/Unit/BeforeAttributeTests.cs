dbd5e2aa65713f9b4148017ee2902e2ae362de15
Oleg Zhuravskiy <zhyravskiy.o@gmail.com>
2026-08-19 12:45:09 +0300

    Added rules what start on b character.

--- Validation.Rules.Tests/Unit/BeforeAttributeTests.cs
+++ Validation.Rules.Tests/Unit/BeforeAttributeTests.cs
@@ -7,16 +7,16 @@ namespace Zhomfr.Validation.Rules.Tests.Unit;
 
 [TestFixture(typeof(DateTime))]
 [TestFixture(typeof(DateTimeOffset))]
-public class AfterAttributeTests<T> where T : ISpanParsable<T>
+public class BeforeAttributeTests<T> where T : ISpanParsable<T>
 {
     private const string VALUE = "2026-04-13 12:21:00";
 
     [Test]
     public void Should_ReturnSuccess_When_ValueIsCorrect()
     {
-        AfterAttribute    attribute         = new(AfterAttributeTests<T>.VALUE);
+        BeforeAttribute   attribute         = new(BeforeAttributeTests<T>.VALUE);
         ValidationContext validationContext = new(new());
-        T                 value             = T.Parse("2026-04-13 12:21:01", CultureInfo.InvariantCulture);
+        T                 value             = T.Parse("2026-04-13 12:20:59", CultureInfo.InvariantCulture);
 
         ValidationResult? result = attribute.GetValidationResult(value, validationContext);
 
@@ -28,7 +28,7 @@ public class AfterAttributeTests<T> where T : ISpanParsable<T>
     {
         AfterAttribute    attribute         = new();
         ValidationContext validationContext = new(new());
-        T                 value             = T.Parse(DateTime.Now.AddDays(1).ToString(CultureInfo.InvariantCulture), CultureInfo.InvariantCulture);
+        T                 value             = T.Parse(DateTime.Now.AddDays(-1).ToString(CultureInfo.InvariantCulture), CultureInfo.InvariantCulture);
 
         ValidationResult? result = attribute.GetValidationResult(value, validationContext);
 
@@ -36,10 +36,10 @@ public class AfterAttributeTests<T> where T : ISpanParsable<T>
     }
 
     [TestCase("2026-04-13 12:21:00")]
-    [TestCase("2026-04-13 12:20:59")]
+    [TestCase("2026-04-13 12:21:01")]
     public void Should_ReturnValidationError_When_ValueIsNotCorrect(string strValue)
     {
-        AfterAttribute attribute = new("2026-04-13 12:21:00") { ErrorMessage = "The date in {0} must be after {1}." };
+        BeforeAttribute attribute = new(BeforeAttributeTests<T>.VALUE) { ErrorMessage = "The date in {0} must be before {1}." };
 
         T value = T.Parse(strValue, CultureInfo.InvariantCulture);
 
@@ -49,26 +49,26 @@ public class AfterAttributeTests<T> where T : ISpanParsable<T>
 
         Assert.That(result, Is.Not.EqualTo(ValidationResult.Success));
 
-        Assert.That(result?.ErrorMessage, Is.EqualTo($"The date in MyProp must be after {AfterAttributeTests<T>.VALUE}."));
-    }
-
-    [Test]
-    public void Should_ReturnSuccess_When_ValueIsNull()
-    {
-        AfterAttribute    attribute         = new(AfterAttributeTests<T>.VALUE);
-        ValidationContext validationContext = new(new());
-
-        ValidationResult? result = attribute.GetValidationResult(null, validationContext);
-
-        Assert.That(result, Is.EqualTo(ValidationResult.Success));
+        Assert.That(result?.ErrorMessage, Is.EqualTo($"The date in MyProp must be before {BeforeAttributeTests<T>.VALUE}."));
     }
 
     [Test]
     public void Should_ThrowUnsupportedTypeException_When_TestedValueIsUnsupportedTypeValue()
     {
-        AfterAttribute    attribute         = new();
+        BeforeAttribute   attribute         = new();
         ValidationContext validationContext = new(new());
 
         Assert.Throws<UnsupportedTypeException>(() => attribute.GetValidationResult(0, validationContext));
+    }
+
+    [Test]
+    public void Should_ReturnSuccess_When_ValueIsNull()
+    {
+        BeforeAttribute   attribute         = new(BeforeAttributeTests<T>.VALUE);
+        ValidationContext validationContext = new(new());
+
+        ValidationResult? result = attribute.GetValidationResult(null, validationContext);
+
+        Assert.That(result, Is.EqualTo(ValidationResult.Success));
     }
 }
